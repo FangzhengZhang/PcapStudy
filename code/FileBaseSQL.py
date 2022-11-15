9a5dcf8c303bb3f987a8d236c3ba884e828eaebf
Frank Zhang <fz2907@vt.edu>
2022-11-14 19:11:16 -0500

    IcedId init data process done

--- code/FileBaseSQL.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def get_json_map_from_DB(db_name):
-    return json.load(open("./DB/" + db_name + ".json"))
-
-def update_json_map_db(db_name, json_map):
-    json.dump(json_map, open("./DB/" + db_name+".json",'w'))
